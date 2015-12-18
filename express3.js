5ddcef5790c794b3d4e2c79092cd782f95f99aa7
Ashley <atan77@gmail.com>
2015-12-18 18:07:49 +1100

    3 Jade
    Create an Express.js app with a home page rendered by Jade template
    engine.
    
    The homepage should respond to /home.
    
    The view should show the
    current date using toDateString.

--- express3.js
+++ express3.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'jade');
-
 // change standard app.set('views', path.join(__dirname, 'templates')) as process.argv[3] has all required text
 app.set('views', process.argv[3]);
 //set app.get with a callback function to render the date from index.jade in the location specified by process.argv[3]
